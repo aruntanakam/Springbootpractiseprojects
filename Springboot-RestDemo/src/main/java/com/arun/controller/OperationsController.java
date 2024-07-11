62b66d425594db9709b3daff31d8211dab14ec49
arun <aruntanakam2000@gmail.com>
2024-07-11 11:20:59 +0530

    third commit added request param and path variables

--- Springboot-RestDemo/src/main/java/com/arun/controller/OperationsController.java
+++ Springboot-RestDemo/src/main/java/com/arun/controller/OperationsController.java
@@ -6,11 +6,14 @@ import java.util.Map;
 import java.util.Set;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arun.model.Address;
@@ -63,7 +66,7 @@ public class OperationsController {
 	}
 	
 	@PostMapping("/save")
-	public ResponseEntity<String> save(@RequestBody Employee e)
+	public ResponseEntity<String> save(@RequestBody Map<String,Object> e)
 	{
 		return new ResponseEntity<String>(e.toString(),HttpStatus.CREATED);
 	}
@@ -73,4 +76,69 @@ public class OperationsController {
 	{
 		return new ResponseEntity<String>(e.toString(),HttpStatus.CREATED);
 	}
+	
+	
+	  @PostMapping("/report") 
+	  public String getReport(@RequestParam(value =
+	  "eno",required=false,defaultValue="0") int no,@RequestParam String name) 
+	  {
+	  return no+" "+name; 
+	  }
+	  
+	  @GetMapping("/report") 
+	  public String getRepor(@RequestParam(value =
+	  "eno",required=false,defaultValue="0") int no,@RequestParam String name)
+	  {
+	  return no+" "+name; 
+	  }
+	  
+	  @PostMapping(value="/reportmap") 
+	  public String getRepors(@RequestParam Map<String,Object> map)
+	  {
+	  return map.get("eno")+" "+map.get("name"); 
+	  }
+	  
+	  
+	  @GetMapping(value={"/report/{eno}/{name}","/report/{eno}"})
+	  public String getvalues(@PathVariable(value="eno")String  no,@PathVariable(required = false) String name)
+	  {
+		  return no+" "+name;  
+	  }
+	  
+	  
+	  @GetMapping("/report/eno/name")
+	  public String getreport1(@PathVariable(value="eno",required = false)String  no,@PathVariable(required = false) String name)
+	  {
+		  return "from 1-->"+no+" "+name;
+	  }
+	  
+	  @GetMapping("/report/{eno}/name")
+	  public String getreport2(@PathVariable(value="eno")String  no,@PathVariable(required = false) String name)
+	  {
+		  return "from 2-->"+no+" "+name;
+	  }
+	 
+	  
+	  @GetMapping("/report/eno/{name}")
+	  public String getreport3(@PathVariable(value="eno",required = false)String  no,@PathVariable(required = false) String name)
+	  {
+		  return "from 3-->"+no+" "+name;
+	  }
+	 
+	  
+	  
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
